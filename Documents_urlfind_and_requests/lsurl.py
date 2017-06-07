53cadd1e47ebe18390822a44d8fc26fa254b5d9e
erlinux <jiwenkangatech@foxmail.com>
2017-06-07 23:32:14 +0800

    Finished

--- Documents_urlfind_and_requests/lsurl.py
+++ Documents_urlfind_and_requests/lsurl.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import re
+import re,requests
 
 class lsurl():
 
@@ -8,9 +8,10 @@ class lsurl():
 
     def Regular_expression(self):
         with open(self.doc_location.encode('utf-8')) as doc_files:
-            regularContect=re.compile(r'((ht|f)(tp+)(s?)(://)[a-zA-Z0-9\.]+(?#url)(:[0-9]+)*(?#port)[^\s]*(?#notspace)(png|jpg|html|txt)+)')
+            regularContect=re.compile(r'((ht|f)(tp+)(s?)(://)[a-zA-Z0-9\.]+(?#url)(:[0-9]+)*(?#port)[^\s]*(?#notspace)(png|jpg|html|txt|\/)+)')
             for iurl in regularContect.findall(doc_files.read()):
-                urls=str(iurl).replace('(','').replace(')','').split(',')[0]
-                print(urls)
+                urls=str(str(iurl).replace('(','').replace(')','').split(',')[0]).replace('\'','')
+                response=requests.get(url=urls)
+                print('链接为: '+str(urls),'状态为: '+str(response.status_code))
 
 lsurl().Regular_expression()
