166b815cfeaa692e041ccc39f3ab8ce99ef086c4
WenkangJi <erlinux@users.noreply.github.com>
2017-06-07 23:49:27 +0800

    Delete ‘/\’

--- Documents_urlfind_and_requests/lsurl.py
+++ Documents_urlfind_and_requests/lsurl.py
@@ -8,7 +8,7 @@ class lsurl():
 
     def Regular_expression(self):
         with open(self.doc_location.encode('utf-8')) as doc_files:
-            regularContect=re.compile(r'((ht|f)(tp+)(s?)(://)[a-zA-Z0-9\.]+(?#url)(:[0-9]+)*(?#port)[^\s]*(?#notspace)(png|jpg|html|txt|\/)+)')
+            regularContect=re.compile(r'((ht|f)(tp+)(s?)(://)[a-zA-Z0-9\.]+(?#url)(:[0-9]+)*(?#port)[^\s]*(?#notspace)(png|jpg|html|txt)+)')
             for iurl in regularContect.findall(doc_files.read()):
                 urls=str(str(iurl).replace('(','').replace(')','').split(',')[0]).replace('\'','')
                 response=requests.get(url=urls)
