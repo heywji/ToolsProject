312f9d3fad5d80eea1580d95232fe921fd393e70
erlinux <jiwenkangatech@foxmail.com>
2017-06-08 00:04:34 +0800

    Fix it

--- Documents_urlfind_and_requests/README.md
+++
@@ -1,1 +0,0 @@
-![](http://irabe.me/pycharm64_2017-06-07_23-21-50.png)
--- Documents_urlfind_and_requests/lsurl.py
+++ Documents_urlfind_and_requests/lsurl.py
@@ -8,6 +8,7 @@ class lsurl():
 
     def Regular_expression(self):
         with open(self.doc_location.encode('utf-8')) as doc_files:
+            # 正则是重点，会不断改进
             regularContect=re.compile(r'((ht|f)(tp+)(s?)(://)[a-zA-Z0-9\.]+(?#url)(:[0-9]+)*(?#port)[^\s]*(?#notspace)(png|jpg|html|txt)+)')
             for iurl in regularContect.findall(doc_files.read()):
                 urls=str(str(iurl).replace('(','').replace(')','').split(',')[0]).replace('\'','')
