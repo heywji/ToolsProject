921f976e6d57eb4175615c1b7f591c2195a2deb6
erlinux <jiwenkangatech@foxmail.com>
2018-04-11 14:01:38 +0800

    Update

--- Finished/Python/WeBots/hurun/spider.py
+++ Finished/Python/WeBots/hurun/spider.py
@@ -17,28 +17,32 @@ class Spider:
         self.nm = list()
         self.url = list()
         self.page = list()
-        for i in data[0::2]:
+        for i in data[0:2]:
             self.nm.append(i[0])
             self.url.append(i[1])
             self.page.append(i[2])
         self.browser = webdriver.Chrome(executable_path=r'/usr/bin/chromedriver')
     def Webot(self,math=None):
         for name,address,page in zip(self.nm,self.url,self.page):
-            self.browser.get(address)
-            spider = Spider()
-            spider.example()
-            for page in range(1,page+1):
-                self.browser.find_element_by_xpath('//*[@id="hs-main"]/div/div[3]/div[3]/div[1]/div[2]/div[4]/div[2]/ul/li[9]/a').click()
-                spider = Spider()
-                spider.example()
-        self.browser.close()
-    def example(self):
-        soup = bs4.BeautifulSoup(self.browser.page_source,"html5lib")
-        data = soup.select("#mytable > tbody > tr")
-        for i in data:
             file = open('/var/www/html/python/data.txt', 'a')
-            file.write(str(i.get_text())+'\n')
+            file.write(str(name)+'\n')
             file.close()
+            self.browser.get(address)
+            soup = bs4.BeautifulSoup(self.browser.page_source,"html5lib")
+            data = soup.select("#mytable > tbody > tr")
+            for i in data:
+                file = open('/var/www/html/python/data.txt', 'a')
+                file.write(str(i.get_text())+'\n')
+                file.close()
+            for page in range(1,page):
+                self.browser.find_element_by_xpath('//*[@id="hs-main"]/div/div[3]/div[3]/div[1]/div[2]/div[4]/div[2]/ul/li[9]/a').click()
+                soup = bs4.BeautifulSoup(self.browser.page_source,"html5lib")
+                data = soup.select("#mytable > tbody > tr")
+                for i in data:
+                    file = open('/var/www/html/python/data.txt', 'a')
+                    file.write(str(i.get_text())+'\n')
+                    file.close()
+        self.browser.close()
 
 if __name__ == '__main__':
     spider = Spider()
